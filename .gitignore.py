13ea2745fa654b354069c29eb358be0dca00f016
_Cauet_Souza <cauetvicto@gmail.com>
2025-12-01 21:15:56 -0300

    Update .gitignore.py
    editado por : Cauet 
    
    Signed-off-by: _Cauet_Souza <cauetvicto@gmail.com>

--- .gitignore.py
+++ .gitignore.py
@@ -1,2 +1,9 @@
 def mostrar_menu():
-    print(jbvjhdfbhjvbjhbdj)
+      print("\n=== SISTEMA DE TAREFAS ===")
+    print("1 - Adicionar nova tarefa")
+    print("2 - Listar tarefas")
+    print("3 - Marcar tarefa como concluída")
+    print("4 - Editar tarefa")
+    print("5 - Remover tarefa")
+    print("6 - Sair")
+    return input("Escolha uma opção: ")
